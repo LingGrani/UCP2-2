16c58440c358a0144e1abda697d1d25b4ac85250
LingGrani <luthfiangger2510@gmail.com>
2024-06-28 09:05:32 +0700

    test deploy 3

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './component/home';
+import About from './component/about';
+import Contact from './component/contact';
 
+const router = createBrowserRouter ([
+  {
+    path: "/UCP2-2/",
+    element: <Home/>,
+    children: [
+      {
+        path: "/UCP2-2/",
+        element: <Home/>
+      },
+      {
+        path: "/UCP2-2/about",
+        element: <About/>
+      },
+      {
+        path: "/UCP2-2/contact",
+        element: <Contact/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
