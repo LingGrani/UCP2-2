8ef75efbbf8ad23e65cf4e67b9b8381e2f25d434
LingGrani <luthfiangger2510@gmail.com>
2024-06-28 09:30:50 +0700

    9 coba pake chatGPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './component/navbar';
-import Footer from './component/footer';
-import Home from './component/home';
-import Contact from './component/contact';
-import About from './component/about';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <Routes>
-          <Route path="/UCP2-2" element={<Home />} />
-          <Route path="/UCP2-2/about" element={<About />} />
-          <Route path="/UCP2-2/contact" element={<Contact />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </Router>
   );
